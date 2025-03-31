cbdc6d3d8751e7096b489ab1cbf9f7a4e210196c
KhoDis <khodzhayarov.a@gmail.com>
2025-03-31 15:53:18 +0300

    ci: add prisma reset before deploy

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+DATABASE_URL="postgresql://bschat_user:bschat_password@db:5432/bschat" npx prisma migrate reset
 DATABASE_URL="postgresql://bschat_user:bschat_password@db:5432/bschat" npx prisma migrate deploy
 DATABASE_URL="postgresql://bschat_user:bschat_password@db:5432/bschat" npm run start
