3ea64ae90bc175c80a4f51d4dd8fba768ee7ad69
KhoDis <khodzhayarov.a@gmail.com>
2025-10-12 23:49:47 +0300

    feat: add linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2021: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: { project: './tsconfig.json' },
+      node: { extensions: ['.ts', '.js', '.json'] },
+    },
+  },
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+        ],
+        pathGroups: [{ pattern: '@/**', group: 'internal', position: 'before' }],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  ignorePatterns: ['dist', 'node_modules', 'coverage'],
+};
